a62e77880703da6be67e73ae070117341031ebca
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2022-10-17 02:48:02 -0600

    Added key and helpful instructions

--- scenes/debug_levels.lua
+++ scenes/debug_levels.lua
@@ -31,7 +31,7 @@ function debug_levels._update()
     -- increment count (shift as a modifier)
     local inc=1
     if btn(globals.btn_z,1) then inc=5 end
-    if btn(globals.btn_right,1) then inc=10 end
+    if btn(globals.btn_right,1) then inc=50 end
 
     -- decrement current option
     if btnp(globals.btn_left) then
@@ -43,7 +43,23 @@ function debug_levels._update()
         menu swap and regeneration
     ]]
 
-    -- swap menus
+    -- change scenes
+    if btn(globals.btn_z,1) then
+        
+        -- return to main menu
+        if btnp(globals.btn_x) then
+            globals.screen=0
+            return
+        end
+
+        -- spawn
+        if level.active~=nil and btnp(globals.btn_z) then
+            level_play.init()
+            return
+        end
+    end--scene changer
+
+    -- swap configuring/preview menus
     if btnp(globals.btn_x) then
         configuring=not configuring
     end
@@ -72,8 +88,21 @@ function debug_levels._draw()
         cls(13)
     	camera(-64,-64)
         level.debug.printsprites()
-        print('press x to go to debug menu',-62,128-81,7)
+        print('key',-62,-62,7)
+        
+        print('enemy count:',-62,-56,1)
+        print('0',-14,-56,5)
+        print('1',-10,-56,10)
+        print('2',-6,-56,9)
+        print('3',-2,-56,8)
+        if level.active~=nil then
+            print('shift+z to spawn character',-62,128-91,11) end
+        print('press x to go to config menu',-62,128-81,7)
         print('press z to regenerate room',-62,128-71,7)
+
+        print('room type:',-62,-50,1)
+        print('start',-22,-50,11)
+        print('end',0,-50,0)
         return
     end--not configuring
 
@@ -82,10 +111,11 @@ function debug_levels._draw()
     camera(0,0)
 
     -- print title
-    local msg='level debug menu'
+    local msg='level sandbox menu'
     print(msg,2,2,9)
     line(2,10,2+(#msg*3),10)
 
+    -- print all options
     local offset=15
     for sel in all(debug_levels.selections) do
         local current=sel==debug_levels.selections[debug_levels.selection]
@@ -104,9 +134,10 @@ function debug_levels._draw()
     end--for
 
     -- print footer
-    print('hold shift to increment +/- 5',2,128-37,7)
-    print('hold f to increment +/- 10',2,128-27,7)
-    print('press x to change to room view',2,128-17,1)
-    print('press z to generate & view room',2,128-7,1)
+    print('hold shift to increment +/- 5',2,128-47,7)
+    print('hold f to increment +/- 50',2,128-37,7)
+    print('press x to change to room view',2,128-27,1)
+    print('press z to generate & view room',2,128-17,1)
+    print('press shift+x for main menu',2,128-7,7)
 
 end--debug_levels._draw()
