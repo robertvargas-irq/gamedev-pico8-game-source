25383931a6b309b676631a15505bb1e5b2cde257
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2022-10-16 21:02:03 -0600

    Added methods to add and check for rooms

--- floor.lua
+++ floor.lua
@@ -1,4 +1,5 @@
 floor={
+    graph=nil,
     rooms=nil,
     root=nil,
     tail=nil,
@@ -8,6 +9,7 @@ floor={
 floor.__index=floor
 
 function floor:new(o)
+    self.graph={} -- x->y->room
     self.rooms={}
     self.root={}
     self.tail={}
@@ -25,3 +27,31 @@ function floor:generate()
     -- generate_from_root(self)
 
 end--floor:generate()
+
+function floor:add_room(r)
+    -- if room already exists, do nothing
+    if self:room_exists(r.x,r.y) then return end
+
+    -- add to list of rooms and inc count
+    add(self.rooms,r)
+    self.r_count+=1
+
+    -- if x value does not exist,
+    -- create new table to support y
+    if self.graph[r.x]==nil then
+        self.graph[r.x]={}
+    end
+    
+    -- map to the correct graph
+    self.graph[r.x][r.y]=r
+
+end--floor:addroom(r)
+
+function floor:room_exists(x,y)
+    return self.graph[x]~=nil and self.graph[x][y]~=nil
+end--floor:room_exists()
+
+function floor:get_room(x,y)
+    if self.graph[x]==nil then return nil end
+    return self.graph[x][y]
+end--floor:get_room()
