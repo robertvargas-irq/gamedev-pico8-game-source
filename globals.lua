dfb6923434cdf6ab449705d001e2c6e109cb9061
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2022-10-16 19:23:00 -0600

    Added helpful keyboard values

--- globals.lua
+++ globals.lua
@@ -1,5 +1,13 @@
 globals={
-    max_rooms=20,
+    btn_left=0,
+    btn_right=1,
+    btn_up=2,
+    btn_down=3,
+    btn_z=4,
+    btn_x=5,
+    camera_size=128,
+    screen=999, -- 0: main_menu | 1: level | 999: debug_levels
+    max_rooms=500,
     difficulty=30,
-    uniformity=10
+    uniformity=100
 }
