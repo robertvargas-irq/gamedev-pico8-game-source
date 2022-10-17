762943dcf1ed768da67c546b65c22ca3afc2a24e
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2022-10-16 19:22:45 -0600

    Created standalone floor generator

--- floor.lua
+++ floor.lua
@@ -1,24 +1,27 @@
 floor={
-    rooms={},
-    root={},
-    tail={},
+    rooms=nil,
+    root=nil,
+    tail=nil,
     max_rooms=0,
     r_count=0
 }
 floor.__index=floor
 
 function floor:new(o)
+    self.rooms={}
+    self.root={}
+    self.tail={}
     return setmetatable(o or {}, self)
 end--floor:new()
 
 function floor:generate()
     -- create new room as root
     local r=room.new(0,0,0)
-    add(level.active.rooms,r)
+    add(self.rooms,r)
     self.root=r
-    self.max_rooms=max_rooms
 
     -- generate the remaining rooms
-    generate_adjacent(self)
-    
+    generate_adjacent(self,r)
+    -- generate_from_root(self)
+
 end--floor:generate()
