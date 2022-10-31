087ef55c8fceb8e3829d81cd85c8853803d7bba8
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2022-10-31 17:17:15 -0600

    Fixed diagonal movement

--- scenes/level_play.lua
+++ scenes/level_play.lua
@@ -12,15 +12,22 @@ end--level_play.init()
 
 -- primary update function
 function level_play._update()
+    local moving = false
     if btn(globals.btn_up) then
+        moving = true
         player_manager.player:move(0,-1)
     elseif btn(globals.btn_down) then
+        moving = true
         player_manager.player:move(0,1)
-    elseif btn(globals.btn_left) then
+    end
+    if btn(globals.btn_left) then
+        moving = true
         player_manager.player:move(-1,0)
     elseif btn(globals.btn_right) then
+        moving = true
         player_manager.player:move(1,0)
-    else
+    end
+    if not moving then
         player_manager.player:move(0,0)
     end
     
@@ -73,9 +80,6 @@ function level_play._draw()
     level_play.render_decor(rx,ry)
     floor_enemies.set_battle_tiles()
     map(0,0,0,0,16,16,1)
-    -- print('standing on:     '..mget(player_manager.player.x/8,player_manager.player.y/8),10,10)
-    -- print('current room:    '..level.active.active_room[1]..','..level.active.active_room[2])
-    -- print('hostiles present:'..#(level.active:get_active_room().enemies or {}))
 
     -- render hostiles in the room
     floor_enemies._draw()
@@ -88,6 +92,19 @@ end--level_player._draw()
 local mid=56
 local p_wall_1=48
 local p_wall_2=72
+local tp_x = 68
+local tp_y = 67
+local wall_x = 65
+local wall_y = 66
+local corner_x = 81
+local corner_y = 82
+local dirs={
+--   dx dy   x1  y1     x2    y2  sprite
+    {-1, 0,   0,mid,     0,mid+8, globals.left_spr }, -- left
+    { 1, 0, 120,mid,   120,mid+8, globals.right_spr}, -- right
+    { 0,-1, mid,  0, mid+8,    0, globals.up_spr   }, -- up
+    { 0, 1, mid,120, mid+8,  120, globals.down_spr } -- down
+}
 function level_play.render_room(x,y)
     -- render corners from TOP ROW -> BOTTOM ROW
     spr(64,0,0,1,1,false,false)
@@ -105,98 +122,88 @@ function level_play.render_room(x,y)
         -- render path walls
         if i==p_wall_1 then
             -- left and right upper
-            spr(82,0,p_wall_1,1,1,false,true)
-            spr(82,120,p_wall_1,1,1,true,true)
+            spr(corner_y,0,p_wall_1,1,1,false,true)
+            spr(corner_y,120,p_wall_1,1,1,true,true)
             -- set map collision
             mset(0,p_wall_1/8,globals.blocking_spr)
             mset(120/8,p_wall_1/8,globals.blocking_spr)
 
             -- top and bottom left-most
-            spr(81,p_wall_1,0,1,1,false,false)
-            spr(81,p_wall_1,120,1,1,false,true)
+            spr(corner_x,p_wall_1,0,1,1,false,false)
+            spr(corner_x,p_wall_1,120,1,1,false,true)
             -- set map collision
             mset(p_wall_1/8,0,globals.blocking_spr)
             mset(p_wall_1/8,120/8,globals.blocking_spr)
         -- render adjacent path walls
         elseif i==p_wall_2 then
             -- left and right lower
-            spr(82,0,p_wall_2,1,1,false,false)
-            spr(82,120,p_wall_2,1,1,true,false)
+            spr(corner_y,0,p_wall_2,1,1,false,false)
+            spr(corner_y,120,p_wall_2,1,1,true,false)
             -- set map collision
             mset(0,p_wall_2/8,globals.blocking_spr)
             mset(120/8,p_wall_2/8,globals.blocking_spr)
 
             -- top and bottom right-most
-            spr(81,p_wall_2,0,1,1,true,false)
-            spr(81,p_wall_2,120,1,1,true,true)
+            spr(corner_x,p_wall_2,0,1,1,true,false)
+            spr(corner_x,p_wall_2,120,1,1,true,true)
             -- set map collision
             mset(p_wall_2/8,0,globals.blocking_spr)
             mset(p_wall_2/8,120/8,globals.blocking_spr)
         -- render walls solidly if not at the half-way point
         elseif i~=mid and i~=mid+8 then
             -- top and bottom rows
-            spr(65,i,0,1,1,false,false)
-            spr(65,i,120,1,1,false,true)
+            spr(wall_x,i,0,1,1,false,false)
+            spr(wall_x,i,120,1,1,false,true)
             -- set map collision
             mset(i/8,0,globals.blocking_spr)
             mset(i/8,120/8,globals.blocking_spr)
 
             -- left and right columns
-            spr(66,0,i,1,1,false,false)
-            spr(66,120,i,1,1,true,false)
+            spr(wall_y,0,i,1,1,false,false)
+            spr(wall_y,120,i,1,1,true,false)
             -- set map collision
             mset(0,i/8,globals.blocking_spr)
             mset(120/8,i/8,globals.blocking_spr)
         -- half-way point; check if pathways are available
         else
-            local dirs={
-            --   dx dy   x1  y1     x2    y2  sprite
-                {-1, 0,   0,mid,     0,mid+8, globals.left_spr }, -- left
-                { 1, 0, 120,mid,   120,mid+8, globals.right_spr}, -- right
-                { 0,-1, mid,  0, mid+8,    0, globals.up_spr   }, -- up
-                { 0, 1, mid,120, mid+8,  120, globals.down_spr } -- down
-            }
             -- check each direction for an adjacent room
             for dir in all(dirs) do
-                -- if there is NOT a room adjacent, spawn rock sprites
-                -- ? update, use walls instead
+                -- if there is NOT a room adjacent, spawn wall sprites
                 local dx,dy,i_x1,i_y1,i_x2,i_y2,sprite=unpack(dir)
                 if level.active:get_room(x+dx,y+dy)==nil then
-                    -- ? use wall sprite
-                    -- spr(2,i_x1,i_y1,1,1)
-                    -- spr(2,i_x2,i_y2,1,1)
+                    -- use wall sprite as blocker
                     if dx == 1 then
-                        spr(66,i_x1,i_y1,1,1,true)
-                        spr(66,i_x2,i_y2,1,1,true)
+                        spr(wall_y,i_x1,i_y1,1,1,true)
+                        spr(wall_y,i_x2,i_y2,1,1,true)
                     elseif dx == -1 then
-                        spr(66,i_x1,i_y1,1,1)
-                        spr(66,i_x2,i_y2,1,1)
+                        spr(wall_y,i_x1,i_y1,1,1)
+                        spr(wall_y,i_x2,i_y2,1,1)
                     elseif dy == 1 then
-                        spr(65,i_x1,i_y1,1,1,false,true)
-                        spr(65,i_x2,i_y2,1,1,false,true)
+                        spr(wall_x,i_x1,i_y1,1,1,false,true)
+                        spr(wall_x,i_x2,i_y2,1,1,false,true)
                     else --dy == -1
-                        spr(65,i_x1,i_y1,1,1)
-                        spr(65,i_x2,i_y2,1,1)
+                        spr(wall_x,i_x1,i_y1,1,1)
+                        spr(wall_x,i_x2,i_y2,1,1)
                     end
                     -- set map collision
                     mset(i_x1/8,i_y1/8,globals.blocking_spr)
                     mset(i_x2/8,i_y2/8,globals.blocking_spr)
-                -- room adjacent, spawn tele-pads
+                -- there is a room adjacent, spawn tele-pads
                 else
                     if dx == 1 then
-                        spr(68,i_x1,i_y1,1,1,true)
-                        spr(68,i_x2,i_y2,1,1,true)
+                        spr(tp_x,i_x1,i_y1,1,1,true)
+                        spr(tp_x,i_x2,i_y2,1,1,true)
                         
                     elseif dx == -1 then
-                        spr(68,i_x1,i_y1,1,1)
-                        spr(68,i_x2,i_y2,1,1)
+                        spr(tp_x,i_x1,i_y1,1,1)
+                        spr(tp_x,i_x2,i_y2,1,1)
                         
                     elseif dy == 1 then
-                        spr(67,i_x1,i_y1,1,1)
-                        spr(67,i_x2,i_y2,1,1)
+                        spr(tp_y,i_x1,i_y1,1,1)
+                        spr(tp_y,i_x2,i_y2,1,1)
                     else --dy == -1
-                        spr(67,i_x1,i_y1,1,1,false,true)
-                        spr(67,i_x2,i_y2,1,1,false,true)
+                        spr(tp_y,i_x1,i_y1,1,1,false,true)
+                        spr(tp_y,i_x2,i_y2,1,1,false,true)
                     end
                     mset(i_x1/8,i_y1/8,sprite)
                     mset(i_x2/8,i_y2/8,sprite)
