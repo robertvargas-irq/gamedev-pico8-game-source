f134ab14f1803cc6bc49183458edd10f287de2db
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2022-11-24 00:24:33 -0700

    hard-coded values to reclaim tokens

--- scenes/merchant_scene.lua
+++ scenes/merchant_scene.lua
@@ -3,14 +3,10 @@ merchant_scene={}
 
 local m={
     sprite=32,
-    x=56,
-    y=56,
     w=2,
     h=2,
     clr_choices={1,2,4,9,15},
     clr=2,
-    speech_x=14,
-    speech_y=5,
     dialogue={
         -- level 1 start
         {
@@ -27,18 +23,12 @@ local m={
     }
 }
 
-local stand={
-    x0=4,
-    x1=123,
-    board_w=5
-}
-
 -- current dialogue option
 local dialogue=1
 
 -- display a merchant's dialogue on screen
 function merchant_scene.speak(text)
-    print(text,m.speech_x,m.speech_y,7)
+    print(text,14,5,7)
 end
 
 function merchant_scene.advance_dialogue()
@@ -70,32 +60,29 @@ end
 
 function merchant_scene._draw()
     cls()
-    -- pset(64,64,1)
-    rectfill(m.x,m.y+1,m.x+15,m.y+9,m.clr) -- merchant skin
-    rectfill(m.x+5,m.y+20,m.x+10,m.y+16,m.clr) -- merchant neck
-    spr(m.sprite,m.x,m.y,2,2) -- merchant head
-    spr(0,m.x,m.y-8,2,1) -- hat
-    spr(18,m.x,m.y+16,2,1) -- collar
 
-    rectfill(stand.x0,79,stand.x1,128,4) -- primary wood
-    rectfill(stand.x0,80,stand.x1,80,15) -- counter aux board
-    rectfill(stand.x0,78,stand.x1,78,15) -- counter
-    rectfill(stand.x0,125,stand.x1,125,15) -- bottom aux board
-    rectfill(stand.x0,127,stand.x1,127,15) -- bottom board
+    rectfill(56,57,71,65,m.clr) -- merchant skin
+    rectfill(61,76,66,72,m.clr) -- merchant neck
+    spr(m.sprite,56,56,2,2) -- merchant head
+    spr(0,56,48,2,1) -- hat
+    spr(18,56,72,2,1) -- collar
+
+    rectfill(4,79,123,128,4) -- primary wood
+    rectfill(4,80,123,80,15) -- counter aux board
+    rectfill(4,78,123,78,15) -- counter
+    rectfill(4,125,123,125,15) -- bottom aux board
+    rectfill(4,127,123,127,15) -- bottom board
 
     -- wooden boards
-    for i=stand.x0,stand.x1,stand.board_w do
+    for i=4,123,5 do
         rectfill(i,80,i,126,15)
     end
-    rectfill(stand.x1,80,stand.x1,126,15)
+    rectfill(123,80,123,126,15)
 
     -- stand poles
-    rectfill(stand.x0,78,stand.x0+5,0,5)
-    rectfill(stand.x1,78,stand.x1-5,0,5)
+    rectfill(4,78,9,0,5)
+    rectfill(123,78,118,0,5)
 
     -- speak text
-    local d=m.dialogue[globals.current_level]
-    merchant_scene.speak(d[dialogue])
-    
-    -- merchant_scene.speak('tHANK YOU FOR DELIVERING \nTHIS PACKAGE SAFELY TO ME.\n\niT MUST HAVE BEEN QUITE \nTHE JOURNEY, i APPRECIATE\nYOUR DEDICATION.')
+    merchant_scene.speak(m.dialogue[globals.current_level][dialogue])
 end
