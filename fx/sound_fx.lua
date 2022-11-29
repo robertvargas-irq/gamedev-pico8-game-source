eb848eec7b1c28dc9813ba5ce6b0396309f08006
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2022-11-28 23:42:27 -0700

    optimized characters

--- fx/sound_fx.lua
+++ fx/sound_fx.lua
@@ -2,47 +2,16 @@
 --[[
     handles any sound effects
 ]]
-
-function sound_ready()
-    sfx(0,-1,0,10)
-end
-
-function sound_enemy_hit()
-    sfx(0,-1,16,12)
-end
-
-function sound_enemy_miss()
-    sfx(1,-1,0,4)
-end
-
-function sound_player_miss()
-    sfx(2,-1,5,2)
-end
-
-function sound_player_hit()
-    sfx(2,-1,0,4)
-end
-
-function sound_select()
-    sfx(0,-1,12,3)
-end
-
-function sound_footstep_1()
-    sfx(1,0,5,2)
-end
-
-function sound_footstep_2()
-    sfx(1,0,8,2)
-end
-
-function sound_tip()
-    sfx(0,-1,29,2)
-end
-
-function sound_merchant_reached()
-    sfx(1,-1,12,4)
-end
-
+function sound_ready() sfx(0,-1,0,10) end
+function sound_enemy_hit() sfx(0,-1,16,12) end
+function sound_enemy_miss() sfx(1,-1,0,4) end
+function sound_player_miss() sfx(2,-1,5,2) end
+function sound_player_hit() sfx(2,-1,0,4) end
+function sound_select() sfx(0,-1,12,3) end
+function sound_footstep_1() sfx(1,0,5,2) end
+function sound_footstep_2() sfx(1,0,8,2) end
+function sound_tip() sfx(0,-1,29,2) end
+function sound_merchant_reached() sfx(1,-1,12,4) end
 function music_room()
     local m={0,3}
     music(-1, 500)
