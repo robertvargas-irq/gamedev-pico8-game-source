47232d39b6583d8ddc01e3fc67b70222f105f9ac
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2022-11-28 18:56:01 -0700

    Removed commented code

--- level.lua
+++ level.lua
@@ -16,7 +16,6 @@ function level.start(l)
     globals.current_level=l
     level.generate(globals.max_rooms+(globals.current_level-1)*5)
     player_manager.get():spawn(60,54)
-    -- level_play.init()
     merchant_scene.init()
 end
 
