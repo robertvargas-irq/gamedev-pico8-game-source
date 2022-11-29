7f66ed5ad09f39299ed7ba09e0ed8d8bbb1fb0f0
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2022-11-29 02:21:53 -0700

    added new music track

--- fx/sound_fx.lua
+++ fx/sound_fx.lua
@@ -8,12 +8,12 @@ function sound_enemy_miss() sfx(1,-1,0,4) end
 function sound_player_miss() sfx(2,-1,5,2) end
 function sound_player_hit() sfx(2,-1,0,4) end
 function sound_select() sfx(0,-1,12,3) end
-function sound_footstep_1() sfx(1,0,5,2) end
-function sound_footstep_2() sfx(1,0,8,2) end
+function sound_footstep_1() sfx(1,-1,5,2) end
+function sound_footstep_2() sfx(1,-1,8,2) end
 function sound_tip() sfx(0,-1,29,2) end
 function sound_merchant_reached() sfx(1,-1,12,4) end
 function music_room()
-    local m={0,3}
+    local m={0,3,8}
     music(-1, 500)
     music(m[current_level])
 end
