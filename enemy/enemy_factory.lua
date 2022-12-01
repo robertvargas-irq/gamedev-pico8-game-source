781fee10c1fe77c210eb703551a2bdc5578c93a9
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2022-12-01 14:50:45 -0700

    balancing changes - Enemies now receive more health based on difficulty

--- enemy/enemy_factory.lua
+++ enemy/enemy_factory.lua
@@ -8,6 +8,8 @@ function enemy_factory.generate(x,y)
         h=2,
         sprite=flr(rnd(3))*2+160,
         damage_bonus=flr(rnd(globals.get_difficulty()/4)),
-        max_health=enemy_factory.minimum_health+flr(rnd(globals.get_difficulty()*2))+flr(globals.get_difficulty()/10)
+        max_health=enemy_factory.minimum_health -- minimum health
+        +flr(rnd(globals.get_difficulty()*3)) -- random value from 0 -> current difficulty * modifier
+        +flr(globals.get_difficulty()/7)     -- current difficulty / 7 (floored)
     })
 end
