05ee3b4d78949408cd5542dd7e9f484c00944f7a
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2022-12-03 04:36:56 -0700

    token optimization

--- enemy/enemy_factory.lua
+++ enemy/enemy_factory.lua
@@ -1,15 +1,15 @@
-enemy_factory={
-    minimum_health=50
-}
+enemy_factory={}
+local minimum_health=50
 
 function enemy_factory.generate(x,y)
+    local diff=globals.get_difficulty()
     return enemy:new(x,y,{
         w=2,
         h=2,
         sprite=flr(rnd(3))*2+160,
-        damage_bonus=flr(rnd(globals.get_difficulty()/4)),
-        max_health=enemy_factory.minimum_health -- minimum health
-        +flr(rnd(globals.get_difficulty()*3)) -- random value from 0 -> current difficulty * modifier
-        +flr(globals.get_difficulty()/7)     -- current difficulty / 7 (floored)
+        damage_bonus=flr(rnd(diff/4)),
+        max_health=minimum_health -- minimum health
+        +flr(rnd(diff*3)) -- random value from 0 -> current difficulty * modifier
+        +flr(diff/7)     -- current difficulty / 7 (floored)
     })
 end
